ef31ffc04c9bd69df61a2b7a547a4ee2f9f52e27
root <root@TENCENT64.site>
2021-07-11 22:30:20 +0800

    add run.sh

---
+++ run.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+cmd=$1
+task=$2
+
+# echo "cmd: $cmd, task: $task"
+
+if [ "$cmd" = 'kill' ]; then
+    if [ -z "$task" ]; then
+        echo "kill muzero"
+        ps -ef|grep muzero |awk '{print $2}'|xargs kill -9
+    fi
+    if [ -n "$task" ]; then
+        echo "kill $task"
+        ps -ef|grep $task |awk '{print $2}'|xargs kill -9
+    fi
+fi
+
+if [ "$cmd" = 'run' ]; then
+    # export http_proxy=
+    # export https_proxy=
+    
+    # >>> conda initialize >>>
+    # !! Contents within this block are managed by 'conda init' !!
+    __conda_setup="$('/root/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f "/root/miniconda3/etc/profile.d/conda.sh" ]; then
+            . "/root/miniconda3/etc/profile.d/conda.sh"
+        else
+            export PATH="/root/miniconda3/bin:$PATH"
+        fi
+    fi
+    unset __conda_setup
+    # <<< conda initialize <<<
+    conda activate torch
+    
+    python /home/jiawei/Code/tx-muzero-hypermodel/muzero.py $task > log.out 2> log.err &
+    # nohup python /home/jiawei/Code/tx-muzero-hypermodel/muzero.py $task > log.out 2> log.err &
+    # echo "run $task"
+fi
+
